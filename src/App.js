7644db3be95edb1087985cf5995fcbf07587b7eb
carter2625 <122479051+carter2625@users.noreply.github.com>
2023-02-12 20:59:16 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+
+function App() {
+  return (
+
+<>
+<div class="card">
+  <h1 class="h1">Philadelphia 76ers</h1>
+  <img class="player" src="https://cdn.nba.com/teams/legacy/www.nba.com/sixers/sites/sixers/files/getty-images-1238023470.jpg" alt="Maxey"></img>
+  <div class="container">
+    <h4 class="h1"><b>Tyrese Maxey, PG</b></h4>
+    <p class="p1">Tyrese Kendrid Maxey, nicknamed "Mad Maxey", is an American professional basketball player for the Philadelphia 76ers of the National Basketball Association.</p>
+    <button class="info">Details</button>
+    </div>
+</div>
+
+  <button class="add">Clone The Card </button>
+    <button class="changeBC">Change Background</button>
+    <button class="changeH">Change Heading</button>
+    <button class="delete">Delete Clone</button>
+  
+</>
+  );
+}
+
+export default App;
